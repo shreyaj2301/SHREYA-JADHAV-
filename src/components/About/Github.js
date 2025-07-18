d873d7b5fab7abc8eb70c366dfc020aaa5d7fad8
Your Name <you@example.com>
2025-07-18 18:56:25 +0530

    first commit

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="shreyaj2301"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#000000ff"
         fontSize={16}
       />
     </Row>
